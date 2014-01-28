313225405a7ab309b7177cda87688ab300f63886
Vaughn Iverson <vsi@Vaughns-Mac-mini-2.local>
2014-01-28 02:07:39 -0800

    Remove unneeded parm and test

--- package.js
+++ package.js
@@ -2,10 +2,8 @@ Package.describe({
   summary: 'Meteor package to wrap TopoJSON: An extension to GeoJSON that encodes topology'
 });
 
-Package.on_use(function(api, where) {
-  if (api.export) {
-    api.export('topojson');
-  }
+Package.on_use(function(api) {
+  api.export('topojson');
   api.add_files('meteor-topojson.js', 'server');
   api.add_files('topojson.js', 'client');
 });
