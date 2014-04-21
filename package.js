c34b39d4e8b270429772fc3526b7a22274f726bc
Vaughn Iverson <vsivsi@yahoo.com>
2014-04-21 11:42:15 -0700

    Update to topojson 1.6.8

--- package.js
+++ package.js
@@ -4,8 +4,8 @@ Package.describe({
 
 Package.on_use(function(api) {
   api.export('topojson');
-  api.add_files('meteor-topojson.js', 'server');
-  api.add_files('topojson.js', 'client');
+  api.add_files('meteor-topojson.js', ['server','client']);
+  // api.add_files('topojson.js', 'client');
 });
 
 Package.on_test(function (api) {
@@ -14,5 +14,5 @@ Package.on_test(function (api) {
 });
 
 Npm.depends({
-  topojson: '1.4.6'
+  topojson: '1.6.8'
 });
